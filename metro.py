35ccccd1b6116d45bb42c3313f3c80e941c3ae23
Nick Heiner <nick.heiner@opower.com>
2013-05-20 16:27:55 -0400

    Adding stubbed out metro.py

---
+++ metro.py
@@ -0,0 +1,21 @@
+"""
+  Return the WMATA json response for the Courthouse metro stop
+"""
+def getJsonForCourthouse():
+  pass
+
+"""
+  Argument: the return value of getJsonForCourthouse()
+
+  Return the time until the next train leaves Westward (towards Clarendon / Vienna)
+"""
+def timeForNextWestTrain(wmataResponse):
+  pass
+
+"""
+  Argument: the return value of getJsonForCourthouse()
+
+  Return the time until the next train leaves Eastwarn (towards Rosslyn / New Carrolton)
+"""
+def timeForNextEastTrain(wmataResponse):
+  pass
